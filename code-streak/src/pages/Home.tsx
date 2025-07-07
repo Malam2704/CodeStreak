b01ed64fe76ed4cf0244e4fa599fb7f154bc4bf5
Mohammed Alam <ma3655@g.rit.edu>
2025-07-06 19:20:31 -0700

    code-streak frontend initial

---
+++ code-streak/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Title } from '@mantine/core';
+
+const Home = () => {
+
+    return (
+        <div>
+            <Title order={1}>Codestreak</Title>
+        </div>
+    );
+};
+
+export default Home;
